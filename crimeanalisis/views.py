6638fbd7bab70677f0ede5d33502750ba97d949d
S0mbr4xc <s3._xc@hotmail.com>
2024-07-14 17:35:24 -0500

    Configuracion de las vistas

--- crimeanalisis/views.py
+++ crimeanalisis/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'crimeanalysis/inicio.html')
+    return render(request, 'crimeanalisis/inicio.html')
 
--- pFinal/urls.py
+++ pFinal/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crimeanalysis/', include('crimeanalysis.urls'))
+    path('crimeanalisis/', include('crimeanalisis.urls'))
 ]
